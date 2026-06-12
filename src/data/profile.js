bdc8bb85eb611f0d8c90ae776f2b38c85f8a902d
Dharmik Rabadiya <djrabadiya2910@gmail.com>
2026-06-12 10:36:45 +0530

    Update profile.js

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,7 +1,7 @@
 const ProfileData = {
   img: require("../assets/Images/hero.png"),
   name: "Dharmik Rabadiya",
-  professions: ["Full Stack Developer", "Enthusiastic Dev", "Gamer"],
+  professions: ["Full Stack Developer", "Enthusiastic Dev"],
   info: [
     "Knack of building web applications using MERN stack.",
     "I enjoy designing tech websites.",
