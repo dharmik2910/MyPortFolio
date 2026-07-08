87241f389145a9ee3b7790cea699612b60c49d5a
Dharmik Rabadiya <djrabadiya2910@gmail.com>
2026-07-08 14:54:00 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,7 +10,7 @@ const ProjectsData = [
     icons: [BiLogoReact, BiLogoRedux, BiLogoTailwindCss, BiLogoMongodb],
     description: "A full-stack investment dashboard with AI-powered insights, portfolio analytics, real-time tracking, and secure authentication using React, Redux, and MongoDB.",
     github: "https://github.com/dharmik2910/Zelbi-AI-Powered-Investment-Dashboard-",
-    demo: "https://zelbi-ai-powered-investment-dashboard.netlify.app/",
+    demo: "http://13.127.181.122/",
   },
   {
     id: "2",
